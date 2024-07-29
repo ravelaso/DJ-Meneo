de4e670680097ab629c39d81d6d4e61d7661e41e
ravelo <ravelaso@outlook.com>
2024-07-29 18:11:31 +0200

    fixed yt-stream

--- src/structure/Player.ts
+++ src/structure/Player.ts
@@ -11,7 +11,7 @@ import {YouTube} from "youtube-sr";
 import Queue from "./Queue";
 // import play from "play-dl";
 import yt from "yt-stream";
-import {audioType, Song, urlType} from "../utils";
+import {audioType, config, Song, urlType} from "../utils";
 import {
     ActivityType,
     ChatInputCommandInteraction,
@@ -38,7 +38,7 @@ export class Player {
         this.currentSong = undefined;
         this.resource = undefined;
         this.guild = guild;
-        yt.setApiKey("AIzaSyDNfC5nO8Z9ZUjo-fesX9v0DKZn6i4EUwo")
+        yt.setApiKey(config.YoutubeAPI)
         yt.setPreference('api','IOS')
         // Player Event
         this.AudioPlayer.on("stateChange", (oldState, newState) => {
